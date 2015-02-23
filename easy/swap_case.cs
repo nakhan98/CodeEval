c2018ad36b0e4c89f0abc359db4b12e8a9dac99e
Nasef Khan <nasef@Asus-Debian>
2015-02-23 23:23:56 +0000

    Added swap case in C#

---
+++ easy/swap_case.cs
@@ -0,0 +1,32 @@
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static char ChangeCase(char x)
+    {
+        if(char.IsUpper(x))
+            return char.ToLower(x);
+        else
+            return char.ToUpper(x);
+    }
+
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (null == line)
+                continue;
+
+            foreach (char x in line)
+            {
+                char newChar = ChangeCase(x);
+                //putchar(newChar);
+                System.Console.Out.Write(newChar);
+            }
+            System.Console.Out.WriteLine("");
+        }
+    }
+}
